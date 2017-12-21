3fcb66d9537514ac70066984b481b623de06200c
Reem Ahmed <34692763+reemahmedosman@users.noreply.github.com>
2017-12-21 13:28:15 +0200

    Update and rename Mainwindow.py to MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Mainwindow.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
+# Form implementation generated from reading ui file 'MainWindow.ui'
 #
 # WARNING! All changes made in this file will be lost!
 
